73e01f573678c8af7df935c961b7b81fb58600c4
RyanGuitar <ryanelsguitar@gmail.com>
2022-01-03 13:55:39 +0200

    changed import url

--- landing.js
+++ landing.js
@@ -1,5 +1,3 @@
-"use strict";
-
 function getId(id) {
   return document.getElementById(id);
 }
@@ -13,7 +11,7 @@ function addClick(id, fn) {
 }
 
 async function importPage(page) {
-  const getPage = await import(`../pages/${page}.js`);
+  const getPage = await import(`../onlineBooking/pages/${page}.js`);
   getPage.default();
 }
 
